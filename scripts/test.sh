fe841d7b6c386e5e6eb9eb7005f258a1388a74b0
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-01-19 10:30:57 +0100

    update

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,12 +4,16 @@
 IMAGE_NAME=$(echo $2 | tr '[:upper:]' '[:lower:]')
 TAG_NAME=$1
 MODULE_NAMES=('Az.Accounts' 'Microsoft.Graph.Authentication' 'Microsoft.Graph.Beta.Users' 'ExchangeOnlineManagement')
-COMMANDS=("Connect-AzAccount" "Connect-MgGraph" "Get-MgBetaUser" "Connect-ExchangeOnline")
+COMMANDS=('Connect-AzAccount' 'Connect-MgGraph' 'Get-MgBetaUser' 'Connect-ExchangeOnline')
 
 # Run the Docker container and execute the command to check for pwsh
 docker run --rm $IMAGE_NAME:$TAG_NAME pwsh -c '$PSVersionTable.PSVersion'
+[ $? -ne 0 ] && echo "Failed to run pwsh" >&2 && exit 1
 
 # Run the Docker container and execute the commands to check for each module
 for i in "${!MODULE_NAMES[@]}"; do
-  docker run --rm $IMAGE_NAME:$TAG_NAME pwsh -c "Import-Module ${MODULE_NAMES[i]}; if (!(Get-Command ${COMMANDS[i]} -ErrorAction SilentlyContinue)) { throw 'Command ${COMMANDS[i]} not found' }"
+  docker run --rm $IMAGE_NAME:$TAG_NAME pwsh -c "if (!(Import-Module ${MODULE_NAMES[i]} -ErrorAction SilentlyContinue)) { Write-Error 'Failed importing module ${MODULE_NAMES[i]}'; exit 1 } elseif ($? && !(Get-Command ${COMMANDS[i]} -ErrorAction SilentlyContinue)) { Write-Error 'Command ${COMMANDS[i]} not found'; exit 1 }"
 done
+[ $? -ne 0 ] && echo "Failed to validate PowerShell modules" >&2 && exit 1
+
+exit 0
