826260d44c3b975fbb7c62c457bd0cec347fec1d
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-01-19 03:57:12 +0100

    bump

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ MODULE_NAMES=('Az.Accounts' 'Microsoft.Graph.Authentication' 'Microsoft.Graph.Be
 COMMANDS=("Connect-AzAccount" "Connect-MgGraph" "Get-MgBetaUser" "Connect-ExchangeOnline")
 
 # Run the Docker container and execute the command to check for pwsh
-#docker run --rm $IMAGE_NAME:$TAG_NAME pwsh -c '\$PSVersionTable.PSVersion'
+docker run --rm $IMAGE_NAME:$TAG_NAME pwsh -c '\$PSVersionTable.PSVersion'
 
 # Run the Docker container and execute the commands to check for each module
 for i in "${!MODULE_NAMES[@]}"; do
