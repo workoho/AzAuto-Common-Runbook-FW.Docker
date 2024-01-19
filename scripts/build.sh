c1bbb4219221e2f1dfbad492b69ef53e9d2e4539
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-01-19 15:09:05 +0100

    fix dockerfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # Define variables
-TARGETPLATFORM=$3
-IMAGE_NAME=$(echo $2 | tr '[:upper:]' '[:lower:]')
-TAG_NAME=$1
+TARGETPLATFORM=$4
+IMAGE_NAME=$(echo $3 | tr '[:upper:]' '[:lower:]')
+TAG_NAME=$2
+DOCKERFILE=$1
 
 # Build the Docker image
 if [[ "$TARGETPLATFORM" == linux/* ]]; then
-    docker build --file Dockerfile.linux --build-arg TARGETPLATFORM=$TARGETPLATFORM -t $IMAGE_NAME:$TAG_NAME .
+    docker build --file $DOCKERFILE --build-arg TARGETPLATFORM=$TARGETPLATFORM -t $IMAGE_NAME:$TAG_NAME .
     if [ $? -ne 0 ]; then
         exit 1
     fi
     exit 0
 fi
 if [[ "$TARGETPLATFORM" == windows/* ]]; then
-    docker build --file Dockerfile.windows --build-arg TARGETPLATFORM=$TARGETPLATFORM -t $IMAGE_NAME:$TAG_NAME .
+    docker build --file $DOCKERFILE --build-arg TARGETPLATFORM=$TARGETPLATFORM -t $IMAGE_NAME:$TAG_NAME .
     if [ $? -ne 0 ]; then
         exit 1
     fi
