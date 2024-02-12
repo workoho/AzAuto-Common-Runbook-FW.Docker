adbfdc9545f1c4aab4a0f1b6344d4642e75c3ed3
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-02-12 20:50:51 +0100

    fix

--- scripts/build-macos.arm64.sh
+++ scripts/build-macos.arm64.sh
@@ -9,8 +9,8 @@ fi
 # Save the current directory and change to the parent directory of the script
 pushd "$(dirname "$0")/.." > /dev/null
 
-# Ensure we return to the original directory on exit
-trap "popd > /dev/null" EXIT
+# Ensure we return to the original directory and log out of Docker on exit
+trap "{ popd > /dev/null; docker logout ghcr.io; }" EXIT
 
 # Log in to the Docker registry
 docker login ghcr.io
@@ -21,7 +21,6 @@ fi
 # Build the Docker image
 docker build --file ./macos/arm64/Dockerfile -t azauto-common-runbook-fw:mariner-2.0-arm64 ./macos/arm64
 if [ $? -ne 0 ]; then
-    docker logout ghcr.io
     exit 1
 fi
 
@@ -33,8 +32,5 @@ docker manifest create --amend ghcr.io/workoho/azauto-common-runbook-fw:latest \
     ghcr.io/workoho/azauto-common-runbook-fw:mariner-2.0-amd64 \
     ghcr.io/workoho/azauto-common-runbook-fw:mariner-2.0-arm64
 docker manifest push --purge ghcr.io/workoho/azauto-common-runbook-fw:latest
-
-# Log out of the Docker registry
-docker logout ghcr.io
 
 exit 0
