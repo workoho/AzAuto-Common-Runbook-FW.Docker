b93062194c027f77a83ca6d0a65b2925e361702f
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-02-12 23:55:51 +0100

    update

--- scripts/build-macos.arm64.sh
+++ scripts/build-macos.arm64.sh
@@ -12,25 +12,25 @@ pushd "$(dirname "$0")/.." > /dev/null
 # Ensure we return to the original directory and log out of Docker on exit
 trap "{ popd > /dev/null; docker logout ghcr.io; }" EXIT
 
+# Build the Docker image
+docker build --file ./macos/arm64/Dockerfile -t azauto-common-runbook-fw:debian-12-arm64 ./macos/arm64
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 # Log in to the Docker registry
 docker login ghcr.io
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Build the Docker image
-docker build --file ./macos/arm64/Dockerfile -t azauto-common-runbook-fw:mariner-2.0-arm64 ./macos/arm64
-if [ $? -ne 0 ]; then
-    exit 1
-fi
-
-docker tag azauto-common-runbook-fw:mariner-2.0-arm64 ghcr.io/workoho/azauto-common-runbook-fw:mariner-2.0-arm64
+docker tag azauto-common-runbook-fw:debian-12-arm64 ghcr.io/workoho/azauto-common-runbook-fw:debian-12-arm64
 
 docker push ghcr.io/workoho/azauto-common-runbook-fw:mariner-2.0-arm64
 
 docker manifest create --amend ghcr.io/workoho/azauto-common-runbook-fw:latest \
     ghcr.io/workoho/azauto-common-runbook-fw:debian-12 \
-    ghcr.io/workoho/azauto-common-runbook-fw:mariner-2.0-arm64
+    ghcr.io/workoho/azauto-common-runbook-fw:debian-12-arm64
 docker manifest push --purge ghcr.io/workoho/azauto-common-runbook-fw:latest
 
 exit 0
