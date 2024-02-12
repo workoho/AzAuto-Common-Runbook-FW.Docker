17f1cc3671a1ea0b22df08cc2973f1809b38d943
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-02-12 10:29:30 +0100

    update

--- scripts/check-upstreams.sh
+++ scripts/check-upstreams.sh
@@ -38,20 +38,26 @@ fi
 IMAGE_NAME="$2"
 modules=("${@:3}")
 
-# Loop over the modules
-for module in "${modules[@]}"; do
-    # Get the latest version from the PowerShell Gallery
-    latest_version=$(docker run --rm $IMAGE_NAME pwsh -Command "Find-Module -Name $module | Select-Object -ExpandProperty Version")
+# Check if the image is available locally
+if docker image inspect $IMAGE_NAME >/dev/null 2>&1; then
+    # Loop over the modules
+    for module in "${modules[@]}"; do
+        # Get the latest version from the PowerShell Gallery
+        latest_version=$(docker run --rm $IMAGE_NAME pwsh -Command "Find-Module -Name $module | Select-Object -ExpandProperty Version")
 
-    # Get the installed version
-    installed_version=$(docker run --rm $IMAGE_NAME pwsh -Command "(Get-Module -ListAvailable -Name $module).Version")
+        # Get the installed version
+        installed_version=$(docker run --rm $IMAGE_NAME pwsh -Command "(Get-Module -ListAvailable -Name $module).Version")
 
-    # Compare the versions
-    if [ "$latest_version" != "$installed_version" ]; then
-        echo "The $module module has been updated. Latest version: $latest_version. Installed version: $installed_version."
-        exit 1
-    fi
-done
+        # Compare the versions
+        if [ "$latest_version" != "$installed_version" ]; then
+            echo "The $module module has been updated. Latest version: $latest_version. Installed version: $installed_version."
+            exit 1
+        fi
+    done
+else
+    echo "The image $IMAGE_NAME was not found, triggering re-build."
+    exit 1
+fi
 
 echo "The upstream PowerShell modules have not changed."
 
