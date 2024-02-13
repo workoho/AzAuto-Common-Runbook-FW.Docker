0546dcdb1ec63138d5941aeb06251848a359c4ed
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-02-13 16:14:41 +0100

    fix

--- scripts/build-macos.arm64.sh
+++ scripts/build-macos.arm64.sh
@@ -7,10 +7,10 @@ if [[ "$(uname -m)" != "arm64" ]]; then
 fi
 
 # Save the current directory and change to the parent directory of the script
-pushd "$(dirname "$0")/.." > /dev/null
+pushd "$(dirname "$0")/.." >/dev/null
 
 # Ensure we return to the original directory and log out of Docker on exit
-trap "{ popd > /dev/null; docker logout ghcr.io; }" EXIT
+trap "{ popd > /dev/null; docker logout ghcr.io; gh auth logout; }" EXIT
 
 # Build the Docker image
 docker build --file ./macos/arm64/Dockerfile -t azauto-common-runbook-fw:debian-12-arm64 ./macos/arm64
@@ -27,10 +27,18 @@ fi
 docker tag azauto-common-runbook-fw:debian-12-arm64 ghcr.io/workoho/azauto-common-runbook-fw:debian-12-arm64
 
 docker push ghcr.io/workoho/azauto-common-runbook-fw:debian-12-arm64
-
-docker manifest create --amend ghcr.io/workoho/azauto-common-runbook-fw:latest \
-    ghcr.io/workoho/azauto-common-runbook-fw:debian-12 \
-    ghcr.io/workoho/azauto-common-runbook-fw:debian-12-arm64
-docker manifest push --purge ghcr.io/workoho/azauto-common-runbook-fw:latest
-
-exit 0
+if [ $? -eq 0 ]; then
+    if command -v gh &>/dev/null; then
+        echo "gh found, running workflow ..."
+        if gh auth login && gh workflow run docker.yml --ref main; then
+            exit 0
+        else
+            exit 1
+        fi
+    else
+        echo "gh not found, please install GitHub CLI"
+        exit 1
+    fi
+else
+    exit 1
+fi
